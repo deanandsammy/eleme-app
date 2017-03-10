d33284c6ca462907a1b7409bab6360d49da60461
elegantspirit <hkissbigapple@163.com>
2017-03-10 13:11:53 +0800

    header-component

--- src/components/header/v-header.vue
+++ src/components/header/v-header.vue
@@ -1,15 +1,45 @@
 <template>
   <div class="header">
-    我是头部
+    <div class="content-wrapper">
+      <div class="avatar">
+        <img :src="seller.avatar" width="64" height="64">
+      </div>
+      <div class="content">
+        <div class="title">
+          <span class="brand"></span>
+          <span class="name">{{seller.name}}</span>
+        </div>
+        <div class="description">
+          {{seller.description}} / {{seller.deliveryTime}}分钟送达
+        </div>
+        <div class="support" v-if="seller.supports">
+          <span class="icon"></span>
+          <span class="text">{{seller.supports[0].description}}</span>
+        </div>
+      </div>
+    </div>
+    <div class="bulletin-wrapper"></div>
   </div>
 </template>
 
 <script>
   export default {
-
+    props: {
+      seller: {
+        type: Object
+      }
+    }
   }
 </script>
 
-<style>
-
+<style lang="stylus" rel="stylesheet/stylus">
+  .header
+    color: #fff
+    background-color: rgba(0, 0, 0, .5)
+    .content-wrapper
+      padding: 24px 12px 18px 24px
+      .avatar
+        display: inline-block
+      .content
+        display: inline-block
 </style>
