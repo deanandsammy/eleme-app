7c4d09398fec6997a2838da99d85619154d8776f
elegantspirit <hkissbigapple@163.com>
2017-03-29 16:10:37 +0800

    goods-component

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="goods">
-    <div class="menu-wrapper"></div>
+    <div class="menu-wrapper">
+      <ul>
+        <li v-for="item in goods" class="menu-item">
+          <span class="text">
+            <span class="icon" v-show="item.type > 0" :class="classMap[item.type]"></span>{{item.name}}
+          </span>
+        </li>
+      </ul>
+    </div>
     <div class="foods-wrapper"></div>
   </div>
 </template>
@@ -25,11 +33,14 @@
           this.goods = response.data
         }
       })
+
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
     }
   }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
   .goods
     display: flex
     position: absolute
@@ -42,4 +53,32 @@
       /* android 机型兼容处理 */
       width: 80px
       background: #f3f5f7
+      .menu-item
+        display: table
+        width: 56px
+        height: 54px
+        line-height: 14px
+        .icon
+          display: inline-block
+          vertical-align: top
+          width: 12px
+          height: 12px
+          margin-right: 2px
+          background-size: 12px 12px
+          background-repeat: no-repeat
+          &.decrease
+            bg-image('decrease_3')
+          &.discount
+            bg-image('discount_3')
+          &.guarantee
+            bg-image('guarantee_3')
+          &.invoice
+            bg-image('invoice_3')
+          &.special
+            bg-image('special_3')
+        .text
+          display: table-cell
+          width: 56px
+          font-size: 12px
+          vertical-align: middle
 </style>
