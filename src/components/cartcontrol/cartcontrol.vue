e0334e583bb25962241a0f751deb1d826d903e1e
elegantspirit <hkissbigapple@163.com>
2017-05-02 11:15:35 +0800

    shopcart-component

--- src/components/cartcontrol/cartcontrol.vue
+++ src/components/cartcontrol/cartcontrol.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="cartcontrol">
-    <div class="cart-decrease" v-show="food.count>0" @click="decreaseCart">-</div>
+    <transition name="my-transition">
+      <div class="cart-decrease" v-show="food.count>0" @click="decreaseCart">-</div>
+    </transition>
     <div class="cart-count" v-show="food.count>0">{{food.count}}</div>
     <div class="cart-add" @click="addCart">+</div>
   </div>
 </template>
 <script type="text/ecmascript-6">
-  import Vue from 'vue'
-
   export default {
     props: {
       food: {
@@ -26,7 +26,7 @@
         }
 
         if (!this.food.count) {
-          Vue.set(this.food, 'count', 1)
+          this.$set(this.food, 'count', 1)
         } else {
           this.food.count++
         }
@@ -38,7 +38,7 @@
         }
 
         if (this.food.count) {
-            this.food.count--
+          this.food.count--
         }
       }
     }
@@ -47,14 +47,20 @@
 <style lang="stylus" rel="stylesheet/stylus">
   .cartcontrol
     font-size: 0
+    .my-transition-enter-active, .my-transition-leave-active
+      transition: all 350ms linear
+    .my-transition-enter, .my-transition-leave-active
+      opacity: 0
+      transform: translate3D(25px, 0, 0)
     .cart-decrease, .cart-add
       display: inline-block
-      width: 15px
-      height: 15px
-      line-height: 15px
+      width: 20px
+      height: 20px
+      line-height: 20px
       font-size: 20px
       color: #fff
       text-align: center
+      vertical-align: middle
       background-color: rgb(0, 160, 220)
       border-radius: 10px
     .cart-count
@@ -62,7 +68,7 @@
       width: 15px
       padding: 1px 3px 0
       line-height: 15px
-      vertical-align: top
+      vertical-align: middle
       text-align: center
       font-size: 10px
       color: rgb(147, 153, 159)
